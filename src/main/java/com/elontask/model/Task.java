6e49fc9510814b48be97b9d842dcfca3a6b8c4b3
Serhii_Riabkin <serhii_riabkin@epam.com>
2020-03-11 15:32:27 +0200

    add priority selection

--- src/main/java/com/elontask/model/Task.java
+++ src/main/java/com/elontask/model/Task.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.Objects;
 
 @Data
@@ -14,12 +16,17 @@ import java.util.Objects;
 @AllArgsConstructor
 @Entity
 public class Task {
+    public static final int DEFAULT_PRIORITY = 3;
+    public static final int MIN_PRIORITY = 1;
+    public static final int MAX_PRIORITY = 5;
     @Id
     @GeneratedValue
     private Long id;
     private String title;
     private String description;
-    private int priority = 3;
+    @Max(MAX_PRIORITY)
+    @Min(MIN_PRIORITY)
+    private int priority = DEFAULT_PRIORITY;
 
     public Task(String title, String description) {
         this.title = title;
--- src/main/java/com/elontask/view/TaskEditor.java
+++ src/main/java/com/elontask/view/TaskEditor.java
@@ -8,18 +8,18 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.spring.annotation.SpringComponent;
 import com.vaadin.flow.spring.annotation.UIScope;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.PostConstruct;
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 @SpringComponent
 @UIScope
-@Slf4j
 public class TaskEditor extends VerticalLayout implements KeyNotifier {
 
     private final TasksService service;
@@ -32,6 +32,7 @@ public class TaskEditor extends VerticalLayout implements KeyNotifier {
 
     private TextField title = new TextField("Title");
     private TextField description = new TextField("Description");
+    private Select<Integer> priority = new Select<>();
     private Button save = new Button("Save", VaadinIcon.CHECK.create());
     private Button cancel = new Button("Cancel");
     private Button delete = new Button("Delete", VaadinIcon.TRASH.create());
@@ -42,48 +43,57 @@ public class TaskEditor extends VerticalLayout implements KeyNotifier {
 
     @PostConstruct
     protected void init() {
-        add(title, description, initActionButtons());
+        initPriority();
+        initActionButtons();
+        add(title, description, priority, new HorizontalLayout(save, cancel, delete));
         binder.bindInstanceFields(this);
+        addKeyPressListener(Key.ENTER, e -> save());
         setSpacing(true);
         setVisible(false);
     }
 
-    protected HorizontalLayout initActionButtons(){
+    private void initPriority() {
+        priority.setLabel("Priority");
+        priority.setItems(IntStream.rangeClosed(Task.MIN_PRIORITY, Task.MAX_PRIORITY).boxed());
+    }
+
+    private void initActionButtons() {
         save.getElement().getThemeList().add("primary");
         save.addClickListener(e -> save());
-        addKeyPressListener(Key.ENTER, e -> save());
 
         cancel.addClickListener(e -> editTask(formTask));
 
         delete.getElement().getThemeList().add("error");
         delete.addClickListener(e -> delete());
-
-        return new HorizontalLayout(save, cancel, delete);
     }
 
-    private void save(){
-        formTask.setTitle(title.getValue());
-        formTask.setDescription(description.getValue());
-        service.addTask(formTask);
-        onChangeStrategy.run();
-    }
-
-    private void delete(){
+    protected void delete() {
         service.deleteTask(formTask.getId());
         onChangeStrategy.run();
     }
 
-    public void editTask(Task task){
-        if (Objects.isNull(task)){
+    protected void save() {
+        service.addTask(updateFromFields(formTask));
+        onChangeStrategy.run();
+    }
+
+    private Task updateFromFields(Task task) {
+        task.setTitle(title.getValue());
+        task.setDescription(description.getValue());
+        task.setPriority(priority.getValue());
+        return task;
+    }
+
+    public void editTask(Task task) {
+        if (Objects.isNull(task)) {
             setVisible(false);
             return;
         }
 
-        if (task.isPersisted()){
+        if (task.isPersisted()) {
             formTask = service.getTaskById(task.getId());
             cancel.setVisible(true);
-        }
-        else {
+        } else {
             formTask = task;
             cancel.setVisible(false);
         }
